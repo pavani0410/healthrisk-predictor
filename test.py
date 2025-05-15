41160a79e42b6c753aa8ac5d759aa5c94bd56af0
Pavani <pavani.sharma21123@gmail.com>
2025-05-15 13:06:30 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,14 @@
 import unittest
-from app import app  # or relevant imports
+from app import app
 
-class BasicTests(unittest.TestCase):
-    def test_example(self):
-        self.assertEqual(1 + 1, 2)
+class AppTestCase(unittest.TestCase):
+    def setUp(self):
+        self.client = app.test_client()
+        self.client.testing = True
+
+    def test_homepage(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
 
 if __name__ == '__main__':
     unittest.main()
